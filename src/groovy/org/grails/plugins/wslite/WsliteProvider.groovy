3612a3eb65122dde541af7df3c8e343c356b387e
Hans Bogaards <hbogaards@gmail.com>
2016-03-11 14:16:07 +0100

    Correcting ancestry (upon https://github.com/cazacugmihai/grails-wslite.git)

--- src/groovy/org/grails/plugins/wslite/WsliteProvider.groovy
+++
@@ -1,9 +0,0 @@
-package org.grails.plugins.wslite
-
-interface WsliteProvider {
-
-    Object withHttp(Map params, Closure closure)
-    Object withRest(Map params, Closure closure)
-    Object withSoap(Map params, Closure closure)
-
-}
